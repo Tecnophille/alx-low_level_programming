04abf4635b1a52f6d75c63bfaf5210266b85ef96
Oliver Samuel <oliversamuel146@gmail.com>
2022-06-02 15:15:52 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\"- Dora Korper, 2015-10-19\n",59);
-		return (1)
+		write(2, "and that piece of art is useful\"- Dora Korper, 2015-10-19\n", 59);
+		return (1);
 }
