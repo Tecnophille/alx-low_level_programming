de7bf0f92a5a0af934865ae96226100ee8ef77cc
Oliver Samuel <oliversamuel146@gmail.com>
2022-06-02 04:23:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,11 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ *     
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+			return (0)
+				;
 }
