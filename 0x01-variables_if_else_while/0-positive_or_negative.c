d5180f774300702f5536e8b51ccaaff51d05a7ae
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-03 14:29:35 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,9 @@ int main(void)
 	{
 	        printf("%d is positive\n", n);
 	}
-
         else if (n == 0)
 	{
-                 printf("%d is zero\n", n);										}
+                printf("%d is zero\n", n);										}
 	else
 	{
 		 printf("%d is negative\n", n);
