f9aff8c36dd617e656e5baa53015577fa82d8060
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-03 14:44:13 +0100

    Prints reversed alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints reversed alphabet.
- *   *
- *    * Return: Always 0 (Success)
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		int i;
+	int i;
 
 	for (i = 122; i > 96; i--)
-			{
-						putchar(i);
-							}
-		putchar('\n');
-			return (0);
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
-
