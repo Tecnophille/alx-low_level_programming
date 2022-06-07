7efce54dd4a8c41051961e755ec9f3be2837fd5d
Oliver Samuel <oliversamuel146@gmail.com>
2022-06-07 21:48:05 +0000

    hdhd

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints natural numbers below 1024 that are
- * multiplies of 3 or 5
- *
+ * main - Prints natural numbers below 1024 that are multiplied of 3 or 5
  * Return: Always 0.
  */
 int main(void)
 {
 	int a, b;
-
+	
 	for (a = 1; a < 1024; a++)
 	{
 		if ((a % 3) == 0 || (a % 5) == 0)
-			b += a;
+		b += a;
 	}
 	printf("%d\n", b);
 	return (0);
