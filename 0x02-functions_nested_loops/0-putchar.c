b2d7c5d7ee38c10138ecaf2b950296f77198a8c7
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-07 13:44:47 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
  * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+  int main(void)
+  
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
