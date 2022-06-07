16a822494840f35e4a4f7f185fd4826377ef6e32
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-07 13:39:31 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,10 +9,9 @@
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 97, 114};
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
+        sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
 		_putchar(str[count]);
