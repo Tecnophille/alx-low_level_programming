062dbea707a54fced7c61c1625cc014d40f110cb
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-09 11:07:02 +0100

    Created

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (i = 48; ch < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
