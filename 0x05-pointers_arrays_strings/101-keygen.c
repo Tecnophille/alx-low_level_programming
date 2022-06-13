c611f5f8c377ed12813a710adf53c7317f975f29
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-13 20:51:02 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - generates keygen.
- * Return: 0 Always.
- */
-int main(void)
-{
-	int r = 0, c = 0;
-	time_t t;
-
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
-}
+#include <stdio.h> 
+ #include <stdlib.h> 
+ #include <time.h> 
+  
+ /** 
+ * main - Entry point 
+ * Return: return 0 
+ */ 
+ int main(void) 
+  { 
+          int contador; 
+          int  checksum; 
+          int  aleatorio; 
+  
+         srand(time(NULL)); 
+         checksum = 2772; 
+         contador = 0; 
+  
+         while (checksum > 122) 
+         { 
+                 aleatorio = (rand() % 100); 
+                 printf("%c", aleatorio); 
+                 checksum -= aleatorio; 
+                 contador++; 
+         } 
+         printf("%c", checksum); 
+         return 0; 
+  }
