a2fc70d9b1ff944a24a12a28f283f7015099115d
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-20 15:31:17 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ int _putchar(char);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+void set_string(char **s, char *to);
 #endif
