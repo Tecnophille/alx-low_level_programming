5ce726ce60316f4ab7175032bf37be726b1b67ba
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-06-21 16:08:54 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,37 +1,32 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
-
 int _strlen_recursion(char *s)
 {
-	if (!*s)
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * p1 - palindrome
- * @s: pointer to string
- * @l: position
- * Return: boolena
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
  */
-
-int p1(char *s, int l)
+int comparator(char *s, int n1, int n2)
 {
-	if (l < 1)
+	if (*(s + n1) == *(s + n2))
 	{
-		return (1);
-	}
-
-	if (*s == *(s + l))
-	{
-		return (p1(s + 1, l - 2));
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
