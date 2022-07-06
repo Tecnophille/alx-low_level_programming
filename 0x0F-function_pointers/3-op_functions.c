9b9757a88ce1aa9da2aeb876be8659cc944f3aa1
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-07-06 11:13:47 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -52,3 +52,20 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
+/**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
